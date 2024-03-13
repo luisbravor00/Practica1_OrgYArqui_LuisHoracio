e51f513a8e0600e221484b6a3b62bda081356813
luisbravor <adrian.bravo@iteso.mx>
2024-03-13 17:20:18 -0600

    Mejora de codigo

--- Practica1_LuisHoracio.asm
+++ Practica1_LuisHoracio.asm
@@ -5,7 +5,7 @@
 #    int numDiscos;
 #    printf("Ingrese el numero de discos: ");
 #    scanf("%d", &numDiscos);
-#    moverTorre(numDiscos, 'O', 'D', 'A');
+#    moverTorre(numDiscos, 'O', 'A', 'D');
 #    return 0;
 #}
 .text
@@ -35,11 +35,11 @@ endfor: addi s1, s1, -4
 	jal moverTorre
 	jal endcode
 	
-#int moverTorre(int altura, char origen, char destino, char intermedio) {
+#int moverTorre(int altura, char origen, char intermedio, char destino) {
 #    if (altura >= 1) {
-#        moverTorre(altura-1,origen,intermedio,destino);
-#        printf("mover disco %d de %c a %c\n", n, desde, hacia);
-#        moverTorre(altura-1,intermedio,destino,origen);
+#        moverTorre(altura-1,origen,destino,intermedio);
+#        printf("mover disco %d de %c a %c\n", altura, origen, destino);
+#        moverTorre(altura-1,intermedio, origen, destino);
 #    }
 #}
 moverTorre: 	addi t0, zero, 1
@@ -97,6 +97,8 @@ moverTorre: 	addi t0, zero, 1
 		addi a7, zero, 11
 		addi a0, zero, 0xA
 		ecall
+		# Mover valores a registros correspondientes
+		
 		# Cambiar valores de a3, a4 y a5
 		add t1, zero, a3	# t1 = origen
 		add a3, zero, a4	# origen = aux
@@ -104,25 +106,25 @@ moverTorre: 	addi t0, zero, 1
 		
 		
 		# SEGUNDA LLAMADA RECURSIVA
-		# push stack -> ra, altura-1, aux, destino, origen
+		# push stack -> ra, altura-1, aux, origen, destino
 		addi sp, sp, -4		# Cargamos ra
 		sw ra, 0(sp)
 		addi sp, sp, -4		# Cargamos altura - 1
 		sw a2, 0(sp)
 		addi sp, sp, -4		# Cargamos auxiliar
 		sw a4, 0(sp)		
-		addi sp, sp, -4		# Carganos destino
-		sw a5, 0(sp)
 		addi sp, sp, -4		# Cargamos origen
 		sw a3, 0(sp)
+		addi sp, sp, -4		# Carganos destino
+		sw a5, 0(sp)
 		# modificacion de argumentos
 		addi a2, a2, -1
 		# llamada recursiva
 		jal moverTorre
-		# pop stack -> origen, destino, aux, altura-1, ra
-		lw a3, 0(sp)		# Sacamos origen
-		addi sp, sp, 4
+		# pop stack -> destino, origen, aux, altura-1, ra
 		lw a5, 0(sp)		# Sacamos destino
+		addi sp, sp, 4
+		lw a3, 0(sp)		# Sacamos origen
 		addi sp, sp, 4
 		lw a4, 0(sp)		# Sacamos aux
 		addi sp, sp, 4
